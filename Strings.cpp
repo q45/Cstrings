29758db929b197662598d952b6646025e7194740
Q <quintinsmith@icloud.com>
2012-09-01 16:09:35 -0600

    Does this go to the simple copy branch

--- Strings.cpp
+++ Strings.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <cstring>
+
 using namespace std;
 
 int main() {
-	char str1[] = "Oh, captain my Captain, our fearful trip is done";
+
+	char str1[] = "Nebraska is beating Mississippi State right now";
 	const int MAX = 80;
 	char str2[MAX];
 
-	
-	for(int i=0; i < strlen(str1); i++) {
-		str2[i] = str1[i];
-		cout << str2[i];
-		str2[i]  = '\0';
-	}//end of for loop
-	cout << endl;
+	strcpy(str2 , str1);
+	cout << str2 << endl;
+
+	cout << str2[4] << endl;
 
 	system("pause");
 	return 0;
-	
-
 }
